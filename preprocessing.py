dfdba80fbc586c2c210c21da6500946b78d3d95a
Dirshaye Y. Faltamo <212802094@ogr.cbu.edu.tr>
2024-03-07 23:54:52 +0300

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -4,6 +4,6 @@ def preprocess_data():
     # Load the raw data
     fitness_data = pd.read_csv('data/fitness_data.csv')
 
-    # Perform any necessary preprocessing (if needed)
+    # Perform any necessary preprocessing  (if needed)
 
     return fitness_data
