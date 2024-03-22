2fa0951af54f31b0c94019f4856044e130dbfe1e
Dirshaye Y. Faltamo <212802094@ogr.cbu.edu.tr>
2024-03-22 23:20:09 +0300

    Update recommendation.py

--- recommendation.py
+++ recommendation.py
@@ -1,4 +1,13 @@
 def recommend_activity(cluster_center):
+    """
+    Recommend an activity based on the characteristics of the cluster center.
+    
+    Args:
+    - cluster_center (Series): Cluster center containing characteristics of the cluster.
+    
+    Returns:
+    - recommended_activity (str): Recommended activity.
+    """
     # Implement your recommendation logic here
     # For example, you could recommend an activity based on the cluster center's characteristics
     return "Cardio"
