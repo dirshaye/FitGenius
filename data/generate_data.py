76d33d0f2028f4be115ea98134f3d1e76f6f8ae8
Dirshaye Y. Faltamo <212802094@ogr.cbu.edu.tr>
2024-04-12 23:56:21 +0300

    Update generate_data.py

--- data/generate_data.py
+++ data/generate_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 # Generating synthetic data
-np.random.seed(42) 
+np.random.seed(42)  
 
 # Define energy levels (0 to 100)
 energy_levels = np.random.randint(0, 101, size=100)
